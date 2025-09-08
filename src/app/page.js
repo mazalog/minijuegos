d62021d3e07a02e53ca543c5c7860943b943cba7
mazalog <opandre123@gmail.com>
2025-09-08 18:28:04 -0400

    settings

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-6 sm:p-10 flex flex-col items-center gap-6">
-      <h1 className="text-2xl font-bold">Juegos</h1>
-      <div className="flex gap-4">
-        <Link className="underline" href="/flappy">Flappy Bird</Link>
-        <Link className="underline" href="/doodle">Doodle Jump</Link>
-        <Link className="underline" href="/dino">Dino</Link>
-        <Link className="underline" href="/road">Road Fighter</Link>
-      </div>
+    <div className="min-h-screen p-6 sm:p-10 flex flex-col items-center justify-center">
+      <Image
+        src="https://firebasestorage.googleapis.com/v0/b/pegalachapa.firebasestorage.app/o/RECURSOS%2FPEGALACHAPABLANCOPNG.png?alt=media&token=bffe20c0-a651-4e13-90f8-648c801c788f"
+        alt="Logo Pegalachapa"
+        width={320}
+        height={320}
+        priority
+      />
     </div>
   );
 }
